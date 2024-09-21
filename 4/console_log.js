73a33aa1cda984c7bf44d20bc84f2a1ce481fded
halac123b <halac123b@gmail.com>
2024-09-21 21:33:09 +0700

    Update m_4

--- 4/console_log.js
+++ 4/console_log.js
@@ -9,10 +9,17 @@ console.log(bar);
 
 // Good Code ✅
 // Khi đó trên console hiển thị được tên của object
-// Thêm custom CSS để nổi bật đánh dấu đoạn log đó
+// Thêm custom CSS để nổi bật đánh dấu đoạn log đó (%c là placeholder của phần CSS)
 console.log("%c My Friends", "color: orange; font-weight: bold;");
 console.log({ foo, bar, baz });
 
 // Các obj có property giống nhau, nên xem như 1 array và hiển thị ra bằng console.table
 // Với các array khác cũng nên log tương tự
 console.table([foo, bar, baz]);
+
+// Start 1 timer với tên gọi loop
+console.time("timer")
+// (do sth..)
+// Dừng timer và in ra tổng thời gian đã chạy đc
+console.timeEnd("timer")
+
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Module-code-Snippets_Javascript
-<p>List of modules:</p>
-<ol>
-  <li>Basic syntax.</li>
-  <li>Word and character counter.</li>
-  <li>Fetch API.</li>
-  <li>JS Best practice tips.</li>
-</ol>
+
+List of modules:
+
+1. Basic syntax.
+2. Word and character counter.
+3. Fetch API.
+4. JS Best practice tips.
